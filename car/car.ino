ddf3de7b8da815a33c6e9575cbeeaeba9e96998b
rainywinter <sunny_up_me@163.com>
2024-10-27 10:20:54 +0800

    basic running

--- car/car.ino
+++ car/car.ino
@@ -1,5 +1,6 @@
 #include <SoftwareSerial.h>
 #include <TaskScheduler.h>
+#include <ArduinoLog.h>
 
 struct MoveController
 {
@@ -20,14 +21,16 @@ void setupMC()
     pinMode(mc.pinRightForwad, OUTPUT);
     pinMode(mc.pinLeftSpeed, OUTPUT);
     pinMode(mc.pinRightSpeed, OUTPUT);
+
+    // Log.verboseln(F("Log mc: %p")  , mc );
 }
 
 struct Distancer
 {
     uint8_t pinTrigger;
     uint8_t pinEcho;
-    long duration; // micro second
-    int distance;  // cm
+    unsigned long duration; // micro second
+    int16_t distance;  // cm
 } distancer = {2, 3, 0, 0};
 void setupDistancer()
 {
@@ -49,18 +52,17 @@ struct Bluetoother
     // do not need to pin mode
     SoftwareSerial ss_bt;
     char readCh;
-} bt = {SoftwareSerial(8, 9),''};
+} bt = {SoftwareSerial(8, 9), '0'}; //(rx,tx);
 
-// void checkDistance();
-// void readBT();
+void checkDistance();
+void readBT();
 
-Task distanceTask(10, TASK_FOREVER, &checkDistance);
+Task distanceTask(5, TASK_FOREVER, &checkDistance);
 Task btTask(5, TASK_FOREVER, &readBT);
 Scheduler runner;
 
 void setupScheduler()
 {
-    // Scheduler.startLoop(checkDistance);
     runner.init();
     runner.addTask(distanceTask);
     runner.addTask(btTask);
@@ -70,15 +72,23 @@ void setupScheduler()
 
 void setup()
 {
+    Serial.println("setup begin");
     // put your setup code here, to run once:
     Serial.begin(9600);
-    bts.begin(9600);
+    bt.ss_bt.begin(9600);
+
+    // while(!Serial && !Serial.available()){}
+    // Log.begin(LOG_LEVEL_VERBOSE, &Serial);
 
     setupDistancer();
     setupBuzzer();
     setupMC();
 
     setupScheduler();
+
+    Serial.println(mc.pinLeftSpeed);
+    Serial.println(mc.pinRightSpeed);
+    Serial.println("setup done");
 }
 
 void loop()
@@ -89,24 +99,29 @@ void loop()
 void checkDistance()
 {
 
-    digitalWrite(trigger, LOW);
+    digitalWrite(distancer.pinTrigger, LOW);
     delayMicroseconds(5);
 
-    digitalWrite(trigger, HIGH);
+    digitalWrite(distancer.pinTrigger, HIGH);
     delayMicroseconds(10);
-    digitalWrite(trigger, LOW);
+    digitalWrite(distancer.pinTrigger, LOW);
 
-    duration = pulseIn(echo, HIGH);
-    distance = duration * 0.034 / 2;
-    // Serial.println("distance: ",distance);
-
-    if (0 < distance && distance <= 30)
+    distancer.duration = pulseIn(distancer.pinEcho, HIGH);
+    Serial.print("pin: ");
+    Serial.println(distancer.pinEcho);
+    Serial.print("duration: ");
+    Serial.println(distancer.duration);
+    distancer.distance = distancer.duration * 0.034 / 2;
+    Serial.print("distance: ");
+    Serial.println(distancer.distance);
+    if (0 < distancer.distance && distancer.distance <= 30)
     {
-        digitalWrite(buzzer, HIGH);
+        digitalWrite(buzzer.pinBuzzer, HIGH);
+        stop();
     }
     else
     {
-        digitalWrite(buzzer, LOW);
+        digitalWrite(buzzer.pinBuzzer, LOW);
     }
 }
 
@@ -117,78 +132,106 @@ void readBT()
         bt.readCh = bt.ss_bt.read();
         Serial.print("bt read:");
         Serial.println(bt.readCh, HEX);
-        runCMD(btChar);
+        runCMD(bt.readCh);
     }
 }
 
 void runCMD(char c)
 {
-    Serial.println("press :", c);
-    stop();
+    Serial.print("press :");
+    Serial.print(c);
+    Serial.print("speed:");
+    Serial.println(mc.speed);
     switch (c)
     {
-    case 'A':
+    case 'A': // 0x41
         turnLeft();
         break;
 
-    case 'D':
+    case 'D': // 0x44
         turnRight();
         break;
 
-    case 'W':
+    case 'W': // 0x57
         moveForward();
         break;
 
-    case 'S':
+    case 'S': // 0x53
         moveBackward();
         break;
-    case 'H':
+    case 'H': // 0x48
         speedUp();
         break;
 
-    case 'L':
+    case 'L': // 0x4c
         speedDown();
         break;
 
     default:
         Serial.println("unsupport cmd.");
+        stop();
         break;
     }
 }
 
+void keepSpeed()
+{
+    uint8_t val = (mc.speed * 255) / 10;
+    Serial.print("keep val");
+    Serial.println(val);
+    analogWrite(mc.pinLeftSpeed, val);
+    analogWrite(mc.pinRightSpeed, val);
+}
+
 void moveForward()
 {
+    Serial.println("forward");
     digitalWrite(mc.pinLeftForward, HIGH);
     digitalWrite(mc.pinRightForwad, HIGH);
+    digitalWrite(mc.pinLeftBackward, LOW);
+    digitalWrite(mc.pinRightBackward, LOW);
+    keepSpeed();
 }
 
 void moveBackward()
 {
+    Serial.println("backward");
+    digitalWrite(mc.pinLeftForward, LOW);
+    digitalWrite(mc.pinRightForwad, LOW);
     digitalWrite(mc.pinLeftBackward, HIGH);
     digitalWrite(mc.pinRightBackward, HIGH);
+
+    keepSpeed();
 }
 
 void turnLeft()
 {
+    Serial.println("left");
     uint8_t fast = (mc.speed * 255) / 10;
-    uint8_t slow = fast - 75 > 0 ? fast - 75 : 0;
+    uint8_t slow = fast - 50 > 0 ? fast - 50 : 0;
     analogWrite(mc.pinLeftSpeed, slow);
     analogWrite(mc.pinRightSpeed, fast);
 }
 void turnRight()
 {
+    Serial.println("right");
     uint8_t fast = (mc.speed * 255) / 10;
-    uint8_t slow = fast - 75 > 0 ? fast - 75 : 0;
+    uint8_t slow = fast - 50 > 0 ? fast - 50 : 0;
     analogWrite(mc.pinLeftSpeed, fast);
-    analogWrite(mc.pinRightSpeed, fast);
+    analogWrite(mc.pinRightSpeed, slow);
 }
 
 void speedUp()
 {
-    mc.speed++;
-    if (mc.speed > 10)
+    Serial.println("speedup");
+
+    if (mc.speed >= 10)
     {
         mc.speed = 10;
+    }
+    else
+    {
+        mc.speed++;
     }
     uint8_t val = (mc.speed * 255) / 10;
     analogWrite(mc.pinLeftSpeed, val);
@@ -197,10 +240,14 @@ void speedUp()
 
 void speedDown()
 {
-    mc.speed--;
-    if (mc.speed < 0)
+    Serial.println("speeddown");
+    if (mc.speed <= 0)
     {
         mc.speed = 0;
+    }
+    else
+    {
+        mc.speed--;
     }
     uint8_t val = (mc.speed * 255) / 10;
     analogWrite(mc.pinLeftSpeed, val);
